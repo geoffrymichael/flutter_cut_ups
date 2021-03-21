f5ed52b1893178f2ca0b46b962ab16ffe3f6e6e7
Geoffry Gambling <groffulet@gmail.com>
2021-03-20 19:23:33 -0700

    moved text spliting logic into its own class

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -15,12 +15,132 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
+class SplitTextLogic {
+  String inputTextFromHomeScreen;
+  final List<String> singleLinesFromHomesScreen;
+  final String dropDownValueFromList;
+  final TextEditingController textController;
+
+  SplitTextLogic(
+      {this.inputTextFromHomeScreen,
+      this.singleLinesFromHomesScreen,
+      this.dropDownValueFromList,
+      this.textController});
+
+  static const Map howToSeparate = {
+    1: 'Single Lines',
+    3: 'Every 3rd Word',
+    5: 'Every 5th Word',
+    0: 'Single Words'
+  };
+
+  void splitTextLogic() {
+    if (dropDownValueFromList == howToSeparate[1]) {
+      print('singleLines');
+      if (inputTextFromHomeScreen == null || inputTextFromHomeScreen.isEmpty) {
+        print("null line");
+      } else {
+        List<String> newTextData = inputTextFromHomeScreen.split('\n');
+        for (String line in newTextData) {
+          singleLinesFromHomesScreen.add(line);
+        }
+        textController.clear();
+        inputTextFromHomeScreen = '';
+      }
+    } else {
+      if (dropDownValueFromList == howToSeparate[3]) {
+        if (inputTextFromHomeScreen == null ||
+            inputTextFromHomeScreen.isEmpty) {
+          print('nothing to cut');
+        } else {
+          if (inputTextFromHomeScreen == null ||
+              inputTextFromHomeScreen.isEmpty) {
+            print('nothing to cut');
+          } else {
+            String singleLine = inputTextFromHomeScreen.replaceAll('\n', ' ');
+
+            var singleWords = singleLine.split(' ');
+
+            var chunks = [];
+            for (var i = 0; i < singleWords.length; i += 3) {
+              chunks.add(singleWords.sublist(
+                  i, i + 3 > singleWords.length ? singleWords.length : i + 3));
+            }
+
+            for (var item in chunks) {
+              final newString = item.join(' ');
+              singleLinesFromHomesScreen.add(newString);
+            }
+            textController.clear();
+            inputTextFromHomeScreen = '';
+          }
+        }
+      } else {
+        if (dropDownValueFromList == howToSeparate[5]) {
+          if (inputTextFromHomeScreen == null ||
+              inputTextFromHomeScreen.isEmpty) {
+            print('nothing to cut');
+          } else {
+            if (inputTextFromHomeScreen == null ||
+                inputTextFromHomeScreen.isEmpty) {
+              print('nothing to cut');
+            } else {
+              String singleLine = inputTextFromHomeScreen.replaceAll('\n', ' ');
+
+              var singleWords = singleLine.split(' ');
+
+              var chunks = [];
+              for (var i = 0; i < singleWords.length; i += 5) {
+                chunks.add(singleWords.sublist(i,
+                    i + 3 > singleWords.length ? singleWords.length : i + 5));
+              }
+
+              for (var item in chunks) {
+                final newString = item.join(' ');
+                singleLinesFromHomesScreen.add(newString);
+              }
+              textController.clear();
+              inputTextFromHomeScreen = '';
+            }
+          }
+        } else {
+          if (dropDownValueFromList == howToSeparate[0]) {
+            if (inputTextFromHomeScreen == null ||
+                inputTextFromHomeScreen.isEmpty) {
+              print('nothing to cut');
+            } else {
+              String singleLine = inputTextFromHomeScreen.replaceAll('\n', ' ');
+              List<String> singleWord = singleLine.split(' ');
+              for (String word in singleWord) {
+                singleLinesFromHomesScreen.add(word);
+              }
+              textController.clear();
+              inputTextFromHomeScreen = '';
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
 class _HomeScreenState extends State<HomeScreen> {
   List<String> singleLines = [];
 
   final TextEditingController _controller = TextEditingController();
 
   String inputText;
+
+  void splitTextClassTest(String dropDownValue) {
+    SplitTextLogic splitLogic = SplitTextLogic(
+        inputTextFromHomeScreen: inputText,
+        singleLinesFromHomesScreen: singleLines,
+        textController: _controller,
+        dropDownValueFromList: dropDownValue);
+
+    splitLogic.splitTextLogic();
+    print("from the split text class");
+  }
 
   void splitText(String dropDownValue) {
     if (dropDownValue == howToSeparate[1]) {
@@ -156,7 +276,7 @@ class _HomeScreenState extends State<HomeScreen> {
           Padding(
             padding: EdgeInsets.only(right: 30),
             child: CutDropDown(
-              cutStyle: splitText,
+              cutStyle: splitTextClassTest,
             ),
           ),
         ],
